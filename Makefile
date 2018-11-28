1a30e2260676ce1a93a246c756d69169d52fb1d3
CSho27 <shorte24@gmail.com>
2018-11-28 00:22:26 -0500

    I have started problem 2

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
-all: prob1.o
+all: prob1.o prob2.o
 	gcc -o prob1 prob1.o -lrt -pthread
+	gcc -o prob2 prob2.o -lrt -pthread
 	
 prob1.o: prob1.c
 	gcc -Wall -Werror -g -c prob1.c -fopenmp
 
+prob2.o: prob2.c
+	gcc -Wall -Werror -g -c prob2.c -fopenmp
 	
 clean:
 	rm -f *.o
 	rm -f prob1
+	rm -f prob2
---
+++ prob2.c
@@ -0,0 +1,124 @@
+//Christopher Shorter
+//OS_HW8
+//Problem 1
+#include <stdio.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define BUFLEN 1024
+
+// Struct for condition variable
+struct condition {
+	sem_t sem; // Semaphore to be initialized to 0
+	int count; // Count of threads waiting
+};
+
+struct condition writer_done;
+struct condition reader_done;
+
+char buffer[BUFLEN];
+int version = 0;
+
+int read_count = 0;
+
+void* reader1();
+void* reader2();
+
+void cwait(struct condition *c); // Semaphore implementation of conditional wait
+void cpost(struct condition *c); // Semaphore implementation of conditional signal
+
+int main(){
+	bzero(buffer, BUFLEN);
+
+	pthread_t rd1_tid;
+	pthread_t rd2_tid;
+
+	pthread_attr_t rd1_attr;
+	pthread_attr_t rd2_attr;
+
+	pthread_attr_init(&rd1_attr);
+	pthread_attr_init(&rd2_attr);
+
+	pthread_create(&rd1_tid, &rd1_attr, reader1, NULL);
+	pthread_create(&rd2_tid, &rd2_attr, reader2, NULL);
+
+	do{
+		
+
+		bzero(buffer, BUFLEN);
+		sprintf(buffer, "version %d: \"string\"", version+1);
+		version++;
+
+		cpost(&writer_done);
+		printf("here\n");
+		fflush(stdout);
+		cwait(&reader_done);
+
+	}while(version<2);
+
+	return 0;
+}
+
+void* reader1(){
+	bool done = false;
+	while(!done){
+
+		cwait(&writer_done);
+
+		printf("here\n");
+		fflush(stdout);
+		read_count++;
+		
+		if(version == 1){
+			printf("Reader #1: %s\n", buffer);
+			fflush(stdout);
+			done = true;
+		}
+
+		read_count--;
+		if(read_count == 0)
+			cpost(&reader_done);
+		
+	}
+	return NULL;
+}
+
+void* reader2(){
+	bool done = false;
+	while(!done){
+		
+		cwait(&writer_done);
+		read_count++;
+		
+		if(version == 1){
+			printf("Reader #1: %s\n", buffer);
+			fflush(stdout);
+			done = true;
+		}
+
+		read_count--;
+		if(read_count == 0)
+			cpost(&reader_done);
+		
+	}
+	return NULL;
+}
+
+// Semaphore implementation of conditional wait
+void cwait(struct condition *c) {
+	c->count++;
+	sem_wait(&(c->sem));
+	c->count--;
+}
+
+// Semaphore implementation of conditional signal
+void cpost(struct condition *c) {
+	if (c->count > 0) {
+		sem_post(&(c->sem));
+	}
+}
